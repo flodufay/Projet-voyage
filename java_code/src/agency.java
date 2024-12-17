8db79026ff5c0e03f55c709b3334da839d57bbd6
Hannah Penz <hpenz@diw.de>
2024-12-17 13:55:36 +0100

    java sources

---
+++ java_code/src/agency.java
@@ -0,0 +1,10 @@
+
+public class agency{
+	
+	public static void main(String[] args) {
+		System.out.println("What would you like to do?");
+		System.out.println("Enter");
+		System.out.println("1 (add a client), 2 (add a reservation), 3 (delete a reservation)");
+		System.out.println("4 (consult reservations by client), 5 (consult reservations by flightnumber");
+	}
+}
